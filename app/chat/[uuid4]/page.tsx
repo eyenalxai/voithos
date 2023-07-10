7ba15ca256cbb201dfba6fdb41a713755c9546c1
Sax Dhiut <mail@iyxeyl.me>
2023-07-10 23:43:27 +0400

    feat: Remove Chat component from Home and redirect to chat page
    Removed the Chat component import and usage from the Home page. Also removed the import of the ChatGPTModel. Instead of rendering the chat on the home page, we now redirect the user to a dedicated chat page identified by a unique UUID. This simplifies the home page and isolates chat functionality to its own route for better separation of concerns.

---
+++ app/chat/[uuid4]/page.tsx
@@ -0,0 +1,22 @@
+import { retrieveUserFromSession } from '@/lib/session'
+import { redirect } from 'next/navigation'
+import Chat from '@/components/chat'
+import { ChatGPTModel } from '@prisma/client'
+
+export type ChatPageProps = {
+  params: {
+    uuid4: string
+  }
+}
+
+export default async function ChatPage({ params }: ChatPageProps) {
+  const user = await retrieveUserFromSession()
+
+  if (!user) {
+    redirect('/sign-in')
+  }
+
+  const model: ChatGPTModel = 'GPT_3_5_TURBO'
+
+  return <Chat uuid4={params.uuid4} model={model} />
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import { retrieveUserFromSession } from '@/lib/session'
 import { redirect } from 'next/navigation'
-import Chat from '@/components/chat'
 import { v4 } from 'uuid'
-import { ChatGPTModel } from '@prisma/client'
 
 export default async function Home() {
   const user = await retrieveUserFromSession()
@@ -12,11 +10,5 @@ export default async function Home() {
   }
 
   const uuid4 = v4()
-  const model: ChatGPTModel = 'GPT_3_5_TURBO'
-
-  return (
-    <main>
-      <Chat uuid4={uuid4} model={model} />
-    </main>
-  )
+  redirect(`/chat/${uuid4}`)
 }
