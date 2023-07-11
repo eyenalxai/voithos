c3d11530aa0d9f3321abe8b2f69b2f918c48f560
Sax Dhiut <mail@iyxeyl.me>
2023-07-12 00:04:03 +0400

    refactor: Combine deleteChat and deleteAllChats mutations
    This commit brings the responsibility of deleting all chats into the deleteChat mutation. This built on the existing deleteChat mutation by allowing the function to delete either an individual chat (when provided with a chat's id) or all chats (when not provided a chat's id). This refactoring reduces redundancy, increases modularity of the mutation function.

--- lib/hook/chats.ts
+++ lib/hook/chats.ts
@@ -7,46 +7,24 @@ export const useChats = () => {
   const queryClient = useQueryClient()
   const { data: chats } = useQuery([CHATS_QUERY_KEY], getChats)
 
-  const { mutate: deleteAllChatsMutation } = useMutation({
-    mutationFn: async () => await deleteAllChats(),
-    onMutate: async () => {
-      await queryClient.cancelQueries([CHATS_QUERY_KEY])
-      const chats = queryClient.getQueryData([CHATS_QUERY_KEY])
-
-      queryClient.setQueryData(
-        [CHATS_QUERY_KEY],
-        (_old: Chat[] | undefined) => {
-          return []
-        }
-      )
-
-      return { chats }
-    },
-    onError: (_error, _var, context) => {
-      if (context) {
-        queryClient.setQueryData([CHATS_QUERY_KEY], context.chats)
-      }
-    },
-    onSettled: async () => {
-      await queryClient.invalidateQueries({
-        queryKey: [CHATS_QUERY_KEY]
-      })
-    }
-  })
-
   const { mutate: deleteChatMutation } = useMutation({
-    mutationFn: async (id: string) => await deleteChat(id),
-    onMutate: async (id: string) => {
+    mutationFn: async (id?: string) =>
+      id !== undefined ? await deleteChat(id) : await deleteAllChats(),
+    onMutate: async (id?: string) => {
       await queryClient.cancelQueries([CHATS_QUERY_KEY])
       const chats = queryClient.getQueryData([CHATS_QUERY_KEY])
 
       queryClient.setQueryData([CHATS_QUERY_KEY], (old: Chat[] | undefined) => {
-        return old ? old.filter(chat => chat.id !== id) : []
+        return id !== undefined
+          ? old
+            ? old.filter(chat => chat.id !== id)
+            : []
+          : []
       })
 
       return { chats }
     },
-    onError: (_error, _var, context) => {
+    onError: (_error, _variables, context) => {
       if (context) {
         queryClient.setQueryData([CHATS_QUERY_KEY], context.chats)
       }
@@ -60,7 +38,7 @@ export const useChats = () => {
 
   return {
     chats,
-    deleteChatMutation,
-    deleteAllChatsMutation
+    deleteChatMutation: deleteChatMutation as (id: string) => Promise<void>,
+    deleteAllChatsMutation: deleteChatMutation as () => Promise<void>
   }
 }
